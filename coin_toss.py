7d87f88e95be72b9a3d0d7e7b1fd1a3fb33a9e21
GOVIND LAL T L <168050736+Govindlaltl@users.noreply.github.com>
2025-06-03 18:44:52 +0530

    Add files via upload

---
+++ coin_toss.py
@@ -0,0 +1,18 @@
+import random
+rand=random.randint(0,1)
+if rand==0:
+    print("""
+            _________
+           /          |
+          |           |
+          |   HEADS   |
+          |    (H)    |
+           \_________/""")
+else:
+    print(""" 
+     _________
+    /         |
+    |          |
+    |   TAILS  |
+    |    (T)   |
+    \_________/""")
